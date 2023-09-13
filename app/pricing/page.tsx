3c3828c0bdd83c88f58d81f4516b4891c0a511a6
mansmahamat <mansour.mahamat.salle@gmail.com>
2023-09-13 15:27:53 +0200

    price

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,4 +1,5 @@
 import { CheckIcon } from "lucide-react"
+import Link from "next/link"
 
 const includedFeatures = [
   "Exclusively Frontend developers",
@@ -54,19 +55,19 @@ export default function Page() {
             <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
               <div className="mx-auto max-w-xs px-8">
                 <p className="text-base font-semibold text-gray-600">
-                  Pay once, own it forever
+                  Each months
                 </p>
                 <p className="mt-6 flex items-baseline justify-center gap-x-2">
                   <span className="text-5xl font-bold tracking-tight text-gray-900">
                     $150
                   </span>
                 </p>
-                <a
-                  href="#"
+                <Link
+                  href="/login"
                   className="mt-10 block w-full rounded-md bg-emerald-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
                 >
                   Get access
-                </a>
+                </Link>
                 <p className="mt-6 text-xs leading-5 text-gray-600">
                   Invoices and receipts available for easy company reimbursement
                 </p>
