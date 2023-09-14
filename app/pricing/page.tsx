bc7a9987b894ea61f3ff46df94e5f3371726a785
mansmahamat <mansour.mahamat.salle@gmail.com>
2023-09-14 15:49:02 +0200

    /dev

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -63,7 +63,7 @@ export default function Page() {
                   </span>
                 </p>
                 <Link
-                  href="/login"
+                  href="/dashboard"
                   className="mt-10 block w-full rounded-md bg-emerald-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
                 >
                   Get access
